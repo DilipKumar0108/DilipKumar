16f7cc218623020dd2fa484592536517016c5669
DilipKumar0108 <panneer.dilip@gmail.com>
2025-11-23 00:15:06 +0530

    problem2 solved

--- src/main/java/org/example/Problem2.java
+++ src/main/java/org/example/Problem2.java
@@ -1,4 +1,20 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Problem2 {
+    static Scanner s=new Scanner(System.in);
+   public static void main(String[] args){
+       System.out.println("Enter the value");
+       int value=s.nextInt();
+       for(int i=1;i<=value;i++){
+           int a=(2*i)-1;
+           System.out.print(a);
+           if(i<value){
+               System.out.print(", ");
+           }
+       }
+
+   }
+
 }
